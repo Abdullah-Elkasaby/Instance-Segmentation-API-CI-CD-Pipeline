230cffe73d5af19dab50fd5e853309324664acc8
97049761+Abdullah-Elkasaby@users.noreply.github.com <terepsichora@gmail.com>
2023-05-08 18:14:05 +0200

    modified main to work from cli using argsparse

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ session = InferenceSession(MODEL_PATH)
 
 
 
-
-
+parser = argparse.ArgumentParser(description="perform instance segementation on an image using fasterRCNN")
 
 
 if __name__ == "__main__":
-   
-    image_path = ""
+    usage = parser.format_usage()
+    usage = usage.replace("usage:", "Usage: python script.py --image_path")
+    parser.usage = usage
+    parser.add_argument("image_path", help="path to the image")
+    args = parser.parse_args()
+    image_path = args.image_path
     org_img, img_data = preprocess_image(image_path)
     boxes, labels, scores = session.run(None, {session.get_inputs()[0].name: img_data})
     display_image(org_img, boxes, labels, scores)
