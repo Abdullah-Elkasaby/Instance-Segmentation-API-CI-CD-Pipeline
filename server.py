1714e86bfc60afcdc33950de3dc752f0d9f25281
97049761+Abdullah-Elkasaby@users.noreply.github.com <terepsichora@gmail.com>
2023-05-08 19:06:36 +0200

    working fastapi server

--- server.py
+++ server.py
@@ -14,3 +14,26 @@ app = FastAPI()
 MODEL_PATH = "models/FasterRCNN-12-qdq.onnx"
 session = InferenceSession(MODEL_PATH)
 
+
+async def run_inference(image_path):
+    org_img, img_data = await preprocess_image(image_path)
+    boxes, labels, scores =  session.run(None, {session.get_inputs()[0].name: img_data})
+    return (org_img, boxes, labels, scores)
+
+
+
+
+@app.post("/upload")
+async def create_upload_file(img: UploadFile = File(...)):
+    org_img, boxes, labels, scores = await run_inference(img.file)
+    imag_ext = img.filename.split('.')[1]
+    rand_image_name = f"{uuid4()}.{imag_ext}"
+    await save_image(rand_image_name, org_img, boxes, labels, scores)
+
+    return FileResponse(rand_image_name)
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
